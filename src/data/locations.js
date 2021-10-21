09d28aef0bc1833b4cd92a43b498bf7f25f0683c
Jamie Lou <jlou711@googlemail.com>
2021-10-21 16:43:01 +0100

    Add data file and remove Location class

---
+++ src/data/locations.js
@@ -0,0 +1,48 @@
+export const locationData = [
+  {
+    title: "100% Pure New Zealand",
+    name: "Queenstown",
+    country: "New Zealand",
+    image:
+      "https://images.unsplash.com/photo-1600466403153-50193d187dde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1674&q=80",
+    map: "https://www.google.com/maps/place/Queenstown,+New+Zealand/@-44.9967032,168.6297593,12z/data=!3m1!4b1!4m5!3m4!1s0xa9d51df1d7a8de5f:0x500ef868479a600!8m2!3d-45.0301511!4d168.6615141",
+    description:
+      "Beautiful everywhere you look, must try Fergburger - the queues are worth it!",
+    img_credits: "https://unsplash.com/@peterluo0113",
+    alt: "Scenic view of Queenstown",
+  },
+  {
+    title: "When life gives you lemons",
+    name: "Amalfi",
+    country: "Italy",
+    image:
+      "https://images.unsplash.com/photo-1510041883570-1c5b27d85cb8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1725&q=80",
+    map: "https://www.google.com/maps/place/84011+Amalfi,+SA,+Italy/@40.6310201,14.5741582,14z/data=!3m1!4b1!4m5!3m4!1s0x133b95b54921c5d1:0x151a2c999fb2aca9!8m2!3d40.6340026!4d14.6026805",
+    description: "Ciao bella! Sunshine, sea and so much good food",
+    img_credits: "https://unsplash.com/@rgaleria",
+    alt: "Colorful buildings in Amalfi",
+  },
+  {
+    title: "Konichiwa",
+    name: "Tokyo",
+    country: "Japan",
+    image:
+      "https://images.unsplash.com/photo-1533050487297-09b450131914?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80",
+    map: "https://www.google.com/maps/place/Tokyo,+Japan/@35.5062907,138.6486406,8z/data=!3m1!4b1!4m5!3m4!1s0x605d1b87f02e57e7:0x2e01618b22571b89!8m2!3d35.6761919!4d139.6503106",
+    description:
+      "Metropolitan city with something new and exciting in every direction",
+    img_credits: "https://unsplash.com/@agk42",
+    alt: "Side street of Tokyo at night",
+  },
+  {
+    title: "Hallo!",
+    name: "Vienna",
+    country: "Austria",
+    image:
+      "https://images.unsplash.com/photo-1587031547865-02677a5dedac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80",
+    map: "https://www.google.com/maps/place/Vienna,+Austria/@48.2208286,16.2399769,11z/data=!3m1!4b1!4m5!3m4!1s0x476d079e5136ca9f:0xfdc2e58a51a25b46!8m2!3d48.2081743!4d16.3738189",
+    description: "So much culture",
+    img_credits: "https://unsplash.com/@anikinearthwalker",
+    alt: "Side street of Tokyo at night",
+  },
+];
