3c70fe22b3cecb3ba6863fe3d28caa9d27171aa8
Jamie Lou <jlou711@googlemail.com>
2021-10-21 15:43:25 +0100

    Add norel to _target blank

--- src/components/place.tsx
+++ src/components/place.tsx
@@ -15,12 +15,12 @@ export function Place(props: Location): JSX.Element {
       <section>
         <div className="card-location">
           <h2>{props.title}</h2>
-          <a href={props.img_credits} target="_blank">
+          <a href={props.img_credits} target="_blank" rel="noreferrer">
             <img src={props.image} alt={props.alt} />
           </a>
           <div className="loc-details">
             📍{" "}
-            <a className="loc-caption" href={props.map} target="_blank">
+            <a className="loc-caption" href={props.map} target="_blank" rel="noreferrer">
               {props.name}, {props.country}
             </a>
             <p className="loc-desc">{props.description}</p>
